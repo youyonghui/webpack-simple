f3413dec4af769604b96fd5973755f57999f115c
leieny <youyonghue@qq.com>
2020-04-15 15:41:20 +0800

    增加html相关插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
 const path = require('path');
 
-// console.log("-----打印配置----------");
-// console.log(path);// 路径
-// console.log(__dirname);// 当前目录下的文件名
-// console.log(__filename) // 表示当前正在执行的脚本的文件名。它将输出文件所在位置的绝对路径，且和命令行参数所指定的文件名不一定相同。 如果在模块中，返回的值是模块文件的路径。
-// console.log("-----打印配置完毕----------");
+// 引入 HtmlWebpackPlugin
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     // entry: './src/index.js', // 单个入口文件
     // 多个入口文件
-    entry:{
-        app:"./src/index.js",
-        print:"./src/print.js"
+    entry: {
+        app: "./src/index.js",
+        print: "./src/print.js"
     },
     output: {
         // filename: 'bundle.js',
-        filename:"[name].bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            // title: '插件测试',
+            template:"./src/index.html"
+        })
+    ],
     module: {
         // rules 规则
         rules: [
